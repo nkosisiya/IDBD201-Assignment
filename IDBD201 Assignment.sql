e7aa15275358c228b4c5e8f171c9fb0805a8eafe
Thecodercg <goliadcassidy000@gmail.com>
2026-04-17 13:51:52 +0200

    Update IDBD201 Assignment.sql

--- IDBD201 Assignment.sql
+++ IDBD201 Assignment.sql
@@ -1,17 +1,17 @@
 --  BOOK
 CREATE TABLE BOOK (
-    ISBN             VARCHAR(20)    PRIMARY KEY,
+    ISBN             VARCHAR(255)    PRIMARY KEY,
     Title            VARCHAR(200)   NOT NULL,
     Price            DECIMAL(10,2),
     Quantity_on_Hand INT            DEFAULT 0,
     Reorder_Level    INT            DEFAULT 5,
-    Publisher_ID     INT,
+    Publisher_ID     INT NOT NULL,
     FOREIGN KEY (Publisher_ID) REFERENCES PUBLISHER(Publisher_ID)
 );
 
 --  BOOK_AUTHOR  (junction: BOOK ↔ AUTHOR)
 CREATE TABLE BOOK_AUTHOR (
-    ISBN      VARCHAR(20) NOT NULL,
+    ISBN      VARCHAR(255) NOT NULL,
     Author_ID INT         NOT NULL,
     PRIMARY KEY (ISBN, Author_ID),
     FOREIGN KEY (ISBN)      REFERENCES BOOK(ISBN),
@@ -23,6 +23,21 @@ CREATE TABLE BookOrder (
     OrderNo     INT         PRIMARY KEY,
     QuantityOrd INT         NOT NULL,
     Received    DATE,
-    ISBN        VARCHAR(20),
+    ISBN        VARCHAR(255) NOT NUll,
     FOREIGN KEY (ISBN) REFERENCES BOOK(ISBN)
-);
+)ENIGINE = InnoDB;
+
+CREATE TABLE BookSale(
+    ISBN     VARCHAR(255) NOT NUll,
+    SaleID    INT  NOT NULL,
+    PRIMARY KEY(ISBN, SaleID),
+    FOREIGN KEY(ISBN) REFERENCES BOOK(ISBN),
+    FOREIGN KEY(SaleID) REFERENCES SALE(SaleID),
+)ENGINE = InnoDB; "Guys please ensure that all the tables have this at the end"
+
+CREATE TABLE SALE(
+    SaleId  INT NOT NULL"Also dont forget to add your NOT NULL/NUll",
+    SaleDate DATE,
+    SaleTotal DECIMALE(10,2),
+    
+)ENGINE = InnoDB
